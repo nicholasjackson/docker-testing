4fe01f05d546c9ba532c17a83db51811a055e5ad
Nic Jackson <jackson.nic@gmail.com>
2015-10-11 20:49:16 +0100

    removed debug from mimic

--- features/steps/mimic.rb
+++ features/steps/mimic.rb
@@ -1,7 +1,5 @@
 require 'rest_client'
 
 Given(/^Mimic is configured with specification$/) do |specification|
-  RestClient.send("post", "#{$MIMIC_SERVER_URI}/api/multi", "#{specification}") do |response, request|
-    puts "#{response.body}"
-  end
+  RestClient.send("post", "#{$MIMIC_SERVER_URI}/api/multi", "#{specification}")
 end
